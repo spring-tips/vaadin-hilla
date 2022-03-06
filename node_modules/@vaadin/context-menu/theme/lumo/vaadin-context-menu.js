eec385c49caffa7e8c868797c9afcf063bfa9457
Josh Long <josh@joshlong.com>
2022-03-05 16:20:12 -0800

    no to node_modules

--- node_modules/@vaadin/context-menu/theme/lumo/vaadin-context-menu.js
+++
@@ -1,4 +0,0 @@
-import './vaadin-context-menu-styles.js';
-import '@vaadin/item/theme/lumo/vaadin-item.js';
-import '@vaadin/list-box/theme/lumo/vaadin-list-box.js';
-import '../../src/vaadin-context-menu.js';
--- node_modules/@vaadin/context-menu/theme/material/vaadin-context-menu.js
+++
@@ -1,4 +0,0 @@
-import './vaadin-context-menu-styles.js';
-import '@vaadin/item/theme/material/vaadin-item.js';
-import '@vaadin/list-box/theme/material/vaadin-list-box.js';
-import '../../src/vaadin-context-menu.js';
